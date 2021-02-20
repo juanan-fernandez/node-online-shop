4fb98b713df982bd541a168464e4c08b54b3ddbc
Juanan Fernandez <juanan.fernandez@gmail.com>
2021-02-20 21:24:39 +0100

    Hasta video 151

--- models/product.js
+++ models/product.js
@@ -1,40 +1,10 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-const db = require('../util/database');
-const Cart = require('./cart');
-
-
-
-module.exports = class Product {
-	constructor(id, title, imageUrl, description, price) {
-		this.id = id;
-		this.title = title;
-		this.imageUrl = imageUrl;
-		this.description = description;
-		this.price = price;
-	}
-
-	save() {
-		if(!this.id) {
-			return db.execute('insert into products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-							[this.title, this.price, this.imageUrl, this.description]
-			);		
-		} else{
-			return db.execute("update products set title=?, price=?, imageUrl=?, description=? where idProduct=?",
-						 [this.title, this.price, this.imageUrl, this.description, this.id]
-			);
-		}
-	
-	}
-
-	static deleteById(id) {
-		return db.execute("delete * from products where idProduct=?", [id]);
-	}
-
-	static fetchAll() {
-		return db.execute('select * from products');
-	}
-
-	static fetchProduct(id) {
-		return db.execute("select * from products where idProduct=?", [id]);
-	}
-};
+const Product = sequelize.define('products', {
+	productId: {type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true},
+	title: {type: Sequelize.STRING, allowNull: false},
+	price: {type: Sequelize.DOUBLE, allowNull: false},
+	imageUrl: {type: Sequelize.STRING, allowNull: false},
+	description: Sequelize.STRING
+});
--- util/database.js
+++ util/database.js
@@ -1,13 +1,11 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const Sequelize = require('sequelize');
 
+const dotenv = require('dotenv');
 dotenv.config();
 
-const cnnPool = mysql.createPool({
-   host: process.env.SQL_HOST,
-   user: process.env.SQL_USER,
-   database: process.env.SQL_DATABASE,
-   password: process.env.SQL_PASSWORD
+const sequelize = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PASSWORD, {
+	dialect: 'mysql',
+	host: process.env.SQL_HOST,
 });
 
-module.exports = cnnPool.promise();
+module.exports = sequelize;
